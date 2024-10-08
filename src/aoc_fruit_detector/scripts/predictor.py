217d84b2b1370e16b1ae0eaf10c953cae5b7cf20
usmanzahidi <usmanzahidi@gmail.com>
2024-10-08 11:48:21 +0100

    changes in predictor script for disabling auto download of assets

--- src/aoc_fruit_detector/scripts/predictor.py
+++ src/aoc_fruit_detector/scripts/predictor.py
@@ -7,9 +7,11 @@ Updated by: Abdurrahman Yilmaz (ayilmaz@lincoln.ac.uk) {06/09/24}
 # config contains default values, urls to datasets, catalog files
 
 import os,traceback,cv2,logging,yaml
+import numpy as np
+
 from detectron_trainer.detectron_trainer     import DetectronTrainer
 from detectron_predictor.detectron_predictor import DetectronPredictor
-from os import listdir
+
 
 from utils.utils import LearnerUtils
 
@@ -34,33 +36,38 @@ name_train                  = config_data['datasets']['train_dataset_name']
 name_test                   = config_data['datasets']['test_dataset_name']
 
 train_image_dir             = config_data['directories']['train_image_dir']
-test_image_dir              = config_data['directories']['train_image_dir']
+test_image_dir              = config_data['directories']['test_image_dir']
 prediction_json_output_dir  = config_data['directories']['prediction_json_dir']
 prediction_image_output_dir = config_data['directories']['prediction_output_dir']
 
 num_classes                 = config_data['training']['number_of_classes']
 epochs                      = config_data['training']['epochs']
+download_assets             = config_data['settings']['download_assets']
 
 # UZ: utils call is made here because we are looping through image directory which is empty in the beginning.
 # This call might be unnecessary in other use cases
 
-downloadUtils=LearnerUtils(config_data)
-downloadUtils.call_download()
+if (download_assets):
+    downloadUtils=LearnerUtils(config_data)
+    downloadUtils.call_download()
 
-rgb_files=listdir(test_image_dir)
+rgb_files = sorted([f for f in os.listdir(test_image_dir) if os.path.isfile(os.path.join(test_image_dir,f))])
 
-def call_predictor():
+
+def call_train()->None:
 
     # instantiation
     det_predictor = DetectronPredictor(config_data)
 
     #loop for generating/saving segmentation output images
+    sample_no=1
     for rgb_file in rgb_files:
         image_file_name=os.path.join(test_image_dir, rgb_file)
         rgb_image   = cv2.imread(image_file_name)
+        rgbd_image = np.dstack([rgb_image,rgb_image[:,:,0]]) #making blue as depth
 
         if rgb_image is None :
-            message = 'path to rgb is invalid or inaccessible for ' + image_file_name
+            message = 'path to rgb is invalid or inaccessible'
             logging.error(message)
 
         # ** main call **
@@ -73,13 +80,12 @@ def call_predictor():
 
             if (__debug__):
 
-                json_annotation_message, predicted_image = det_predictor.get_predictions_image(rgb_image,
-                                                                                               prediction_json_output_file,
-                                                                                               image_file_name)
+                json_annotation_message,predicted_image,depth_masks = det_predictor.get_predictions_image(rgbd_image, prediction_json_output_file,
+                                                                                image_file_name,sample_no)
+
             else:
-                dummy_image_id = 1
-                json_annotation_message, predicted_image = det_predictor.get_predictions_message(rgb_image,
-                                                                                                 dummy_image_id)
+                dummy_image_id=1
+                json_annotation_message, predicted_image,depth_masks = det_predictor.get_predictions_message(rgbd_image, dummy_image_id)
             # Use output json_annotation_message,predicted_image as per requirement
             # In Optimized (non-debug) mode predicted_image is None
 
@@ -88,13 +94,18 @@ def call_predictor():
         except Exception as e:
             logging.error(e)
             print(traceback.format_exc()) if __debug__ else print(e)
-
-def call_trainer():
+        sample_no += 1
+    call_trainer(False,True) # call for evaluation
+def call_trainer(resumeType=False,skipTraining=False)->None:
 
     try:
         detTrainer=DetectronTrainer(config_data)
-        aoc_trainer=detTrainer.train_model(resumeType=False) # set resumeType=True when continuing training on top of parly trained models
-        detTrainer.evaluate_model(aoc_trainer.model)
+        aoc_trainer=detTrainer.train_model(resumeType=resumeType,skipTraining=skipTraining) # set resumeType=True when continuing training on top of parly trained models
+        detTrainer.evaluate_model(aoc_trainer.model);
     except Exception as e:
         logging.error(e)
         print(traceback.format_exc()) if __debug__ else print(e)
+
+
+
+
